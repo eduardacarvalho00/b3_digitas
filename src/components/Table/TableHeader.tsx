8dec31587ee963714c5eaf7e01f1cb3253dd9f71
eduarda <eduardacarvalho9833@gmail.com>
2025-02-02 20:20:45 -0300

    feat: add button for refresh get

--- src/components/Table/TableHeader.tsx
+++ src/components/Table/TableHeader.tsx
@@ -11,20 +11,20 @@ export const TableHeader = ({ handleSort }: TableHeaderProps) => {
       {["code", "value", "timestamp"].map((header) => (
         <th
           key={header}
-          className="p-4 text-left border-b border-gray-700 bg-gray-800 bg-opacity-50"
+          className="p-4 text-left border-b border-gray-700 bg-gray-800 bg-opacity-50 capitalize"
         >
           <button
-            className="flex items-center text-sm font-medium uppercase tracking-wider text-gray-400"
+            className="flex items-center text-sm font-medium tracking-wider text-gray-400"
             onClick={() => handleSort(header as keyof CurrencyRate)}
           >
             {header === "code"
               ? "Currency"
               : header === "value"
-              ? "value (BRL)"
+              ? "Value (BRL)"
               : "Date/Time"}
 
             {header !== "timestamp" && (
-              <ArrowUpDown size={16} className="ml-1" />
+              <ArrowUpDown size={16} className="ml-1 cursor-pointer" />
             )}
           </button>
         </th>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,22 @@
+import { RefreshCcw } from "lucide-react";
 import Header from "../../components/Header";
 import { Table } from "../../components/Table";
 import { useCurrency } from "../../queries/useCurrency";
 
 export const Home = () => {
-  const { data, isFetching } = useCurrency();
+  const { data, isFetching, refetch } = useCurrency();
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-800 text-gray-100 flex flex-col">
       <Header />
       <main className="flex-grow flex items-center justify-center p-4">
         <div className="max-w-6xl w-full">
+          <div
+            className="flex gap-4 font-semibold text-gray-500 mb-4 w-full justify-end cursor-pointer"
+            onClick={() => refetch()}
+          >
+            <p>Refresh</p>
+            <RefreshCcw />
+          </div>
           {isFetching && (
             <div className="w-full flex justify-center">
               <p className="font-semibold text-gray-500">Loading...</p>
